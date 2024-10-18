f633235b4f7c5191d1e828d69ae2ab6f0670b3f7
kiwa <1430266059@qq.com>
2024-10-18 14:24:01 +0800

    kiwa

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    styled: true,
+    themes: ["light", "dark", "cupcake", "valentine","dracula"],
+    rtl: false
+  },
+}
